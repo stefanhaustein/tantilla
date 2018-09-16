94834cf381f7e6584184fac36ff735e0ae51093a
stefan.haustein@gmail.com <stefan.haustein@gmail.com>
2018-09-16 23:10:35 +0200

    fix builtins, expand blocks example

--- core/src/main/java/org/kobjects/asde/lang/Builtin.java
+++ core/src/main/java/org/kobjects/asde/lang/Builtin.java
@@ -31,7 +31,7 @@ public enum Builtin implements Function {
       for (int i = 0; i < parameters.length; i++) {
         parameters[i] = new Parameter(String.valueOf((char) ('a' + i)), parameterTypes[i]);
       }
-      this.signature = new FunctionType(name().endsWith("$") ? Types.STRING : Types.NUMBER);
+      this.signature = new FunctionType(name().endsWith("$") ? Types.STRING : Types.NUMBER, parameterTypes);
     }
 
   @Override
