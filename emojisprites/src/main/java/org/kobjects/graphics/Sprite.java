dccc54498402d7d806bd0cb7003ad16f32cf1e80
stefan.haustein@gmail.com <stefan.haustein@gmail.com>
2019-01-14 23:38:05 +0100

    some progress with sprite text simplification

--- emojisprites/src/main/java/org/kobjects/graphics/Sprite.java
+++ emojisprites/src/main/java/org/kobjects/graphics/Sprite.java
@@ -1,14 +1,9 @@
 package org.kobjects.graphics;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.util.TypedValue;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -16,14 +11,9 @@ public class Sprite extends PositionedViewHolder<ImageView> {
 
     public static final String DEFAULT_FACE = "\ud83d\ude03";
 
-    Paint backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    Paint borderPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-    TextView labelView;
-    TextView bubble;
+    TextBox label;
+    TextBox bubble;
     private float size;
-    private String label;
-    private String text;
     private String face;
     private float angle;
 
@@ -36,18 +26,12 @@ public class Sprite extends PositionedViewHolder<ImageView> {
         view.wrapped.setAdjustViewBounds(true);
         view.wrapped.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
+        /*
         backgroundPaint.setStyle(Paint.Style.FILL);
         backgroundPaint.setColor(Color.WHITE);
         borderPaint.setStyle(Paint.Style.STROKE);
         borderPaint.setColor(Color.BLACK);
         borderPaint.setStrokeWidth(Dimensions.dpToPx(context, 1));
-
-        labelView = new TextView(context);
-        //  labelView.setBackgroundColor(Color.WHITE);
-        labelView.setTextColor(Color.BLACK);
-        labelView.setVisibility(View.GONE);
-
-        bubble = new TextView(context);
 
         int basePx = Dimensions.dpToPx(context, 4);
 
@@ -58,7 +42,7 @@ public class Sprite extends PositionedViewHolder<ImageView> {
         bubbleDrawable.strokePaint = borderPaint;
         bubble.setBackground(bubbleDrawable);
         bubble.setTextColor(Color.BLACK);
-        bubble.setVisibility(View.GONE);
+        bubble.setVisibility(View.GONE);*/
 
         setFace(DEFAULT_FACE);
 
@@ -66,11 +50,11 @@ public class Sprite extends PositionedViewHolder<ImageView> {
 
     }
     public String getLabel() {
-        return label;
+        return label == null ? "" : label.getText();
     }
 
     public String getText() {
-        return text;
+        return bubble == null ? "" : bubble.getText();
     }
 
     public String getFace() {
@@ -88,35 +72,8 @@ public class Sprite extends PositionedViewHolder<ImageView> {
 
     @Override
     public void syncUi() {
-
-        if (labelView.getParent() == null) {
-           // screen.view.addView(view, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            screen.view.addView(labelView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            screen.view.addView(bubble, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        }
-
         if (textDirty) {
             textDirty = false;
-
-            if (label == null || label.isEmpty()) {
-                labelView.setText("");
-                labelView.setVisibility(View.GONE);
-            } else {
-                labelView.setTextSize(TypedValue.COMPLEX_UNIT_PX, 5 * screen.scale);
-                labelView.setVisibility(View.VISIBLE);
-                labelView.setText(" " + label + " ");
-                setX(x);
-            }
-
-            if (text == null || text.isEmpty()) {
-                bubble.setText("");
-                bubble.setVisibility(View.GONE);
-            } else {
-                bubble.setVisibility(View.VISIBLE);
-                bubble.setText(text);
-                bubble.measure(View.MeasureSpec.AT_MOST | Dimensions.dpToPx(bubble.getContext(), 160), View.MeasureSpec.UNSPECIFIED);
-                bubble.getLayoutParams().width = bubble.getMeasuredWidth();
-            }
             view.wrapped.setImageDrawable(Emojis.getDrawable(view.getContext(), face));
         }
 
@@ -132,19 +89,12 @@ public class Sprite extends PositionedViewHolder<ImageView> {
         float screenX = x * screen.scale + anchor.view.getWidth() / 2;
 
         view.setTranslationX(screenX - intrinsicWidth / 2);
-        labelView.setTranslationX(screenX - labelView.getPaint().measureText(labelView.getText().toString()) / 2);
-        bubble.setTranslationX(screenX - bubble.getMeasuredWidth() / 2);
 
-        float heightPx = intrinsicHeight * view.getScaleY();
         float scrY = anchor.view.getHeight() / 2 - y * screen.scale;
 
         view.setTranslationY(scrY - intrinsicHeight / 2);
 
-        labelView.setTranslationY(scrY + heightPx / 2);
-        bubble.setTranslationY(scrY - bubble.getMeasuredHeight() - 20 - heightPx / 2);
         view.setTranslationZ(z);
-        labelView.setTranslationZ(z);
-        bubble.setTranslationZ(z);
     }
 
 
@@ -157,22 +107,35 @@ public class Sprite extends PositionedViewHolder<ImageView> {
         return true;
     }
     public boolean setLabel(String label) {
-        if (Objects.equals(label, this.label)) {
+        if (Objects.equals(label, getLabel())) {
             return false;
         }
-        this.label = label;
-        textDirty = true;
-        requestSync();
+        if (this.label == null) {
+            this.label = new TextBox(screen);
+            this.label.setAnchor(this);
+            this.label.setTextColor(0xff000000);
+            this.label.setFillColor(0xffffffff);
+            this.label.setLineColor(0xff000000);
+            this.label.setY((getHeightForAnchoring() + this.label.getHeightForAnchoring()) / -2);
+        }
+        this.label.setText(label);
         return true;
     }
 
     public boolean setText(String text) {
-        if (Objects.equals(text, this.text)) {
+        if (Objects.equals(text, getText())) {
             return false;
         }
-        this.text = text;
-        textDirty = true;
-        requestSync();
+        if (this.bubble == null) {
+            this.bubble = new TextBox(screen);
+            this.bubble.setAnchor(this);
+            this.bubble.setTextColor(0xff000000);
+            this.bubble.setFillColor(0xffffffff);
+            this.bubble.setLineColor(0xff000000);
+            this.bubble.setY(5 + (getHeightForAnchoring() + bubble.getHeightForAnchoring()) / 2);
+            this.bubble.setCornerRadius(5);
+        }
+        this.bubble.setText(text);
         return true;
     }
 
